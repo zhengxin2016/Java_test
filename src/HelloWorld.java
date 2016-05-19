954ccf4442c35958c719bf480829bc88d25951fb
zhengxin <zhengxin@ncic.ac.cn>
2016-05-19 19:37:03 +0800

    test merge

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world!!!");
 		System.out.println("hello ketty!!!");
+		System.out.println("how are you");
 	}
 
 }
